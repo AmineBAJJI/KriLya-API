7850781377c754423c36fa9bec28e91be97e925b
AmineBAJJI <aminebajji18@gmail.com>
2023-08-14 11:26:04 +0100

    export, import plugin configuration

---
+++ src/api/manifacturer/controllers/manifacturer.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * manifacturer controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::manifacturer.manifacturer');
---
+++ src/api/manifacturer/routes/manifacturer.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * manifacturer router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::manifacturer.manifacturer');
